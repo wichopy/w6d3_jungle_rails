0d20a34bb77e022aee75beed7d64d3b06ea64830
william chou <w.chou06@gmail.com>
2017-03-16 11:12:36 -0400

    Implemented user auth using bcrypt and has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,17 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  #Vanilla login routes.
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
