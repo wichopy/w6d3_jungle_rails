618bf78e217fa3c763bba5e699284823fd74ee6c
william chou <w.chou06@gmail.com>
2017-03-16 19:48:43 -0400

    Implemented basic http login.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-  
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
     @categories = Category.order(:name).all
   end
